f1d6bb20869750e4250f44e96a3568e73aff10a1
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-11-28 16:40:12 +0300

    feat(srvr): Пустой Spring проект

---
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/BachelorThesisServerApplication.java
@@ -0,0 +1,13 @@
+package ru.zalimannard.bachelorthesisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorThesisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorThesisServerApplication.class, args);
+	}
+
+}
