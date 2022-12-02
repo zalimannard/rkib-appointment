a48395debbf8acb283dd36b37e517efa0b0a1268
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-12-03 01:35:27 +0300

    feat(server): Репозитории-заглушки всех сущностей

---
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/repositories/PlanElementRepository.java
@@ -0,0 +1,34 @@
+package ru.zalimannard.bachelorthesisserver.repositories;
+
+import org.springframework.stereotype.Repository;
+import ru.zalimannard.bachelorthesisserver.entities.Employee;
+
+import java.util.List;
+
+@Repository
+public class PlanElementRepository implements BaseRepository<Employee> {
+    @Override
+    public void create(Employee employee) {
+
+    }
+
+    @Override
+    public Employee retrieve(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> retrieveAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Employee employee) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
