99d98058607e236f4b7993aad6ee574ea53eb2f9
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-12-03 20:41:05 +0300

    feat(srvr): Репозитории для обращений

--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/repositories/VisitStatusRepository.java
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/repositories/VisitStatusRepository.java
@@ -1,9 +1,15 @@
 package ru.zalimannard.bachelorthesisserver.repositories;
 
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import ru.zalimannard.bachelorthesisserver.entities.DoctorNote;
 import ru.zalimannard.bachelorthesisserver.entities.VisitStatus;
+import ru.zalimannard.bachelorthesisserver.exceptions.ConflictException;
+import ru.zalimannard.bachelorthesisserver.exceptions.NotFoundException;
+import ru.zalimannard.bachelorthesisserver.exceptions.NotModifiedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,26 +22,114 @@ public class VisitStatusRepository implements BaseRepository<VisitStatus> {
 
     @Override
     public void create(VisitStatus visitStatus) {
+        String query = """
+                INSERT INTO
+                    visit_statuses(
+                        visit_type_id,
+                        visit_status_name
+                    )
+                VALUES
+                    (?, ?)
+                """;
+        Object[] parameters = new Object[]{
+                visitStatus.typeId(),
+                visitStatus.name()
+        };
 
+        jdbcOperations.update(query, parameters);
     }
 
     @Override
     public VisitStatus retrieve(int id) {
-        return null;
+        String query = """
+                SELECT
+                    visit_status_id,
+                    visit_type_id,
+                    visit_status_name
+                FROM
+                    visit_statuses
+                WHERE
+                    visit_status_id = ?
+                """;
+        Object[] parameters = new Object[]{
+                id
+        };
+
+        SqlRowSet sqlRowSet = jdbcOperations.queryForRowSet(query, parameters);
+        if (sqlRowSet.next()) {
+            return new VisitStatus(
+                    sqlRowSet.getInt("visit_status_id"),
+                    sqlRowSet.getInt("visit_type_id"),
+                    sqlRowSet.getString("visit_status_name")
+            );
+        } else {
+            throw new NotFoundException("Статус посещения не найден");
+        }
     }
 
     @Override
     public List<VisitStatus> retrieveAll() {
-        return null;
+        String query = """
+                SELECT
+                    visit_status_id,
+                    visit_type_id,
+                    visit_status_name
+                FROM
+                    visit_statuses
+                """;
+
+        SqlRowSet sqlRowSet = jdbcOperations.queryForRowSet(query);
+        List<VisitStatus> response = new ArrayList<>();
+        while (sqlRowSet.next()) {
+            response.add(new VisitStatus(
+                    sqlRowSet.getInt("visit_status_id"),
+                    sqlRowSet.getInt("visit_type_id"),
+                    sqlRowSet.getString("visit_status_name")
+            ));
+        }
+        return response;
     }
 
     @Override
     public void update(VisitStatus visitStatus) {
+        String query = """
+                UPDATE
+                    visit_statuses
+                SET
+                    visit_type_id = ?,
+                    visit_status_name = ?
+                WHERE
+                    visit_status_id = ?
+                """;
+        Object[] parameters = new Object[]{
+                visitStatus.typeId(),
+                visitStatus.name(),
+                visitStatus.id()
+        };
 
+        int jdbcOperationsResult = jdbcOperations.update(query, parameters);
+        boolean isSuccessfullyUpdated = (jdbcOperationsResult == 1);
+        if (!isSuccessfullyUpdated) {
+            throw new NotModifiedException("Статус посещения не изменён");
+        }
     }
 
     @Override
     public void delete(int id) {
+        String query = """
+                DELETE FROM
+                    visit_statuses
+                WHERE
+                    visit_status_id = ?
+                """;
+        Object[] parameters = new Object[]{
+                id
+        };
 
+        int jdbcOperationsResult = jdbcOperations.update(query, parameters);
+        boolean isSuccessfullyDeleted = (jdbcOperationsResult == 1);
+        if (!isSuccessfullyDeleted) {
+            throw new ConflictException("Статус посещения не удалён");
+        }
     }
 }
--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/repositories/VisitTypeRepository.java
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/repositories/VisitTypeRepository.java
@@ -1,10 +1,14 @@
 package ru.zalimannard.bachelorthesisserver.repositories;
 
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import ru.zalimannard.bachelorthesisserver.entities.DoctorNote;
 import ru.zalimannard.bachelorthesisserver.entities.VisitType;
+import ru.zalimannard.bachelorthesisserver.exceptions.NotFoundException;
 import ru.zalimannard.bachelorthesisserver.exceptions.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -22,12 +26,49 @@ public class VisitTypeRepository implements BaseRepository<VisitType> {
 
     @Override
     public VisitType retrieve(int id) {
-        return null;
+        String query = """
+                SELECT
+                    visit_type_id,
+                    visit_type_name
+                FROM
+                    visit_types
+                WHERE
+                    visit_type_id = ?
+                """;
+        Object[] parameters = new Object[]{
+                id
+        };
+
+        SqlRowSet sqlRowSet = jdbcOperations.queryForRowSet(query, parameters);
+        if (sqlRowSet.next()) {
+            return new VisitType(
+                    sqlRowSet.getInt("visit_type_id"),
+                    sqlRowSet.getString("visit_type_name")
+            );
+        } else {
+            throw new NotFoundException("Тип обращения не найден");
+        }
     }
 
     @Override
     public List<VisitType> retrieveAll() {
-        return null;
+        String query = """
+                SELECT
+                    visit_type_id,
+                    visit_type_name
+                FROM
+                    visit_types
+                """;
+
+        SqlRowSet sqlRowSet = jdbcOperations.queryForRowSet(query);
+        List<VisitType> response = new ArrayList<>();
+        while (sqlRowSet.next()) {
+            response.add(new VisitType(
+                    sqlRowSet.getInt("visit_type_id"),
+                    sqlRowSet.getString("visit_type_name")
+            ));
+        }
+        return response;
     }
 
     @Override
