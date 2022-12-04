0abce4a0c97b8624f922afded8dcdb1049e2bb9a
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-12-04 16:25:38 +0300

    feat(srvr): Репозиторий для Service

--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/entities/Service.java
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/entities/Service.java
@@ -2,13 +2,13 @@ package ru.zalimannard.bachelorthesisserver.entities;
 
 import java.util.Objects;
 
-public record Services(int id, String name) implements BaseEntity {
+public record Service(int id, String name) implements BaseEntity {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Services services = (Services) o;
-        return id == services.id;
+        Service service = (Service) o;
+        return id == service.id;
     }
 
     @Override
