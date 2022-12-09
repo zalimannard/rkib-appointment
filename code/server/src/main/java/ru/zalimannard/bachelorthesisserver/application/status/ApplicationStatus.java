d406e5ac2980a979314b7ee52419f1407f55772a
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-12-09 17:42:32 +0300

    feat(srvr): Сущность и DTO для Статуса обращения

--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/application/status/ApplicationStatus.java
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/application/status/ApplicationStatus.java
@@ -1,18 +1,43 @@
 package ru.zalimannard.bachelorthesisserver.application.status;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
 import java.util.Objects;
 
-public record ApplicationStatus(int id, int typeId, String name) {
+@Entity
+@Table(name = "application_statuses")
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ApplicationStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "type_code", nullable = false)
+    private ApplicationStatusType type;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ApplicationStatus that = (ApplicationStatus) o;
-        return id == that.id;
+        return Objects.equals(getId(), that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(getId());
     }
 }
