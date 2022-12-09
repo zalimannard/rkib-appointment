0ea0f861cc0a03435df6af2f53d74a3370ac5c78
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-12-09 13:36:01 +0300

    feat(srvr): Сущность и DTO для Доктора

--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/doctor/Doctor.java
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/doctor/Doctor.java
@@ -1,9 +1,37 @@
 package ru.zalimannard.bachelorthesisserver.doctor;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
 import java.util.Objects;
 
-public record Doctor(int id, String lastName, String firstName, String middleName, String login,
-                     String passwordHash, String passwordSalt) {
+@Entity
+@Table(name = "doctors")
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotNull
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotNull
+    @Column(name = "middle_name", nullable = false)
+    private String middleName;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
