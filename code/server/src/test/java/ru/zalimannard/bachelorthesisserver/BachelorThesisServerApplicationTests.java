b88505c7fa8ea88ca1dbc293d8f22545c1180a01
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-12 13:04:51 +0300

    feat: Конфигурационные фаилы Docker

--- code/server/src/test/java/ru/zalimannard/bachelorthesisserver/BachelorThesisServerApplicationTests.java
+++ code/server/src/test/java/ru/zalimannard/bachelorthesisserver/BachelorThesisServerApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.zalimannard.bachelorthesisserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BachelorThesisServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
