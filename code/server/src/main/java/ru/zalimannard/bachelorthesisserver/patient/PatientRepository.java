df095d7f9f62e6efd578fe597a3f09a42144ce98
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-25 21:03:14 +0300

    feat: Фильтр для Patient

--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/patient/PatientRepository.java
+++ code/server/src/main/java/ru/zalimannard/bachelorthesisserver/patient/PatientRepository.java
@@ -1,9 +1,10 @@
 package ru.zalimannard.bachelorthesisserver.patient;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends CrudRepository<Patient, String> {
+public interface PatientRepository extends JpaRepository<Patient, String> {
 
 }
