8dac7154f27e2cc7ef7785990bbf489abf62c68c
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-04-07 19:43:43 +0300

    build: Пустой Spring-проект

---
+++ backend/src/main/java/ru/zalimannard/rkibappointmentbackend/RkibAppointmentBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.zalimannard.rkibappointmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkibAppointmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RkibAppointmentBackendApplication.class, args);
+    }
+
+}
