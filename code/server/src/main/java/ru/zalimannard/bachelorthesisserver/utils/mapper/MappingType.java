e70277661b497dff9756051aa77249bacb05eda0
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-04-07 18:26:55 +0300

    revert: Удалить всё

--- code/server/src/main/java/ru/zalimannard/bachelorthesisserver/utils/mapper/MappingType.java
+++
@@ -1,6 +0,0 @@
-package ru.zalimannard.bachelorthesisserver.utils.mapper;
-
-public enum MappingType {
-    DEFAULT,
-    FORCE
-}
--- code/server/src/test/java/ru/zalimannard/bachelorthesisserver/BachelorThesisServerApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package ru.zalimannard.bachelorthesisserver;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BachelorThesisServerApplicationTests {
-
-}
