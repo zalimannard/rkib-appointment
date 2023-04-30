182c5bffe2ff9f75b5ab5873f934150f2653a047
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-04-30 19:45:23 +0300

    feat: Простая страница входа

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -15,7 +15,7 @@
           Welcome to Vuetify 3 Beta
         </h1>
 
-          <h4>Vite Preview</h4>
+        <h4>Vite Preview</h4>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
@@ -93,65 +93,65 @@
   </v-container>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue'
+<script lang="ts">
+import { defineComponent } from "vue";
 
 // Logo
-import logo from '../assets/logo.svg'
+import logo from "../assets/logo.svg";
 
 export default defineComponent({
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
-  data () {
+  data() {
     return {
       ecosystem: [
         {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
+          text: "vuetify-loader",
+          href: "https://github.com/vuetifyjs/vuetify-loader/tree/next"
         },
         {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify/tree/next',
+          text: "github",
+          href: "https://github.com/vuetifyjs/vuetify/tree/next"
         },
         {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
+          text: "awesome-vuetify",
+          href: "https://github.com/vuetifyjs/awesome-vuetify"
+        }
       ],
       importantLinks: [
         {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
+          text: "Chat",
+          href: "https://community.vuetifyjs.com"
         },
         {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
+          text: "Made with Vuetify",
+          href: "https://madewithvuejs.com/vuetify"
         },
         {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
+          text: "Twitter",
+          href: "https://twitter.com/vuetifyjs"
         },
         {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
+          text: "Articles",
+          href: "https://medium.com/vuetify"
+        }
       ],
       logo,
       whatsNext: [
         {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com',
+          text: "Explore components",
+          href: "https://vuetifyjs.com"
         },
         {
-          text: 'Roadmap',
-          href: 'https://vuetifyjs.com/en/introduction/roadmap/',
+          text: "Roadmap",
+          href: "https://vuetifyjs.com/en/introduction/roadmap/"
         },
         {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }
-  },
-})
+          text: "Frequently Asked Questions",
+          href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+        }
+      ]
+    };
+  }
+});
 </script>
